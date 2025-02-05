b973243254c5a5ebea023bc10d80388e97f2531a
Rick Elrod <rick@elrod.me>
2025-02-05 01:25:22 +0100

    level1: Add a few more platforms to play with
    Signed-off-by: Rick Elrod <rick@elrod.me>

--- src/level1.rs
+++ src/level1.rs
@@ -36,4 +36,55 @@ pub fn setup(mut commands: Commands) {
             ..default()
         },
     ));
+
+    commands.spawn((
+        Sprite {
+            color: Color::srgb(0.8, 0.6, 0.3),
+            ..default()
+        },
+        Transform {
+            translation: Vec3::new(350.0, -45.0, 0.0),
+            scale: Vec3::new(10.0, 30.0, 1.0),
+            ..default()
+        },
+        Platform,
+        Collider {
+            size: Vec2::new(10.0, 30.0),
+            ..default()
+        },
+    ));
+
+    commands.spawn((
+        Sprite {
+            color: Color::srgb(0.8, 0.6, 0.3),
+            ..default()
+        },
+        Transform {
+            translation: Vec3::new(-180.0, -80.0, 0.0),
+            scale: Vec3::new(10.0, 30.0, 1.0),
+            ..default()
+        },
+        Platform,
+        Collider {
+            size: Vec2::new(10.0, 30.0),
+            ..default()
+        },
+    ));
+
+    commands.spawn((
+        Sprite {
+            color: Color::srgb(0.8, 0.6, 0.3),
+            ..default()
+        },
+        Transform {
+            translation: Vec3::new(-300.0, -15.0, 0.0),
+            scale: Vec3::new(10.0, 30.0, 1.0),
+            ..default()
+        },
+        Platform,
+        Collider {
+            size: Vec2::new(10.0, 30.0),
+            ..default()
+        },
+    ));
 }
